4e12a7708729f6ace53f6a6647172a373b9edef0
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-08-08 12:12:26 +0530

    hundred random memes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { data } from "./data.js";
 
 const app = express();
 dotenv.config();
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.get("/",(req,res) => {
-    res.send("Helo to sample Api");
+    res.status(200).json(data);
     res.end();
 })
 
