b2837fcf1a862e2c9b3f512ebb277a54c20660b7
tarunkumarchourasiya <tkcthousand@gmail.com>
2023-07-18 17:28:55 +0530

    first commit

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const PORT = process.env.PORT || 5000;
 app.get("/",(req,res) => {
     res.status(200).json(data);
     res.end();
-})
+});
 
 app.listen(PORT,() => console.log(`server is running on port 5000`));
